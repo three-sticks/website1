bb775889bea515387e868d7af69b2865dce819af
three-sticks <rhardy2020@gmail.com>
2026-01-02 15:05:18 -0500

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,15 +1,10 @@
 import "./styles.scss";
-import "bootstrap/dist/js/bootstrap.bundle.min.js";
 import initScrollReveal from "./scripts/scrollReveal";
 import initTiltAnimation from "./scripts/tiltAnimation";
 import { targetElements, defaultProps } from "./data/scrollRevealConfig";
-// index.js or main.js
-
-
 
 initScrollReveal(targetElements, defaultProps);
 initTiltAnimation();
-
 
 document.addEventListener('DOMContentLoaded', () => {
     const toggle = document.querySelector('.nav-toggle');
