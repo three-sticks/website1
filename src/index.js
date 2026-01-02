a4168f809d0eeb2926c87e36d3e7a6003a67034b
three-sticks <rhardy2020@gmail.com>
2026-01-01 21:32:40 -0500

    Update Animation

--- src/index.js
+++ src/index.js
@@ -1,23 +1,25 @@
 import "./styles.scss";
 import initScrollReveal from "./scripts/scrollReveal";
-import initTiltEffect from "./scripts/tiltAnimation";
+import initTiltAnimation from "./scripts/tiltAnimation";
 import { targetElements, defaultProps } from "./data/scrollRevealConfig";
 // index.js or main.js
 
 
 
 initScrollReveal(targetElements, defaultProps);
-initTiltEffect();
+initTiltAnimation();
 
 
 document.addEventListener('DOMContentLoaded', () => {
     const toggle = document.querySelector('.nav-toggle');
     const menu = document.querySelector('.nav-menu');
-  
-    toggle.addEventListener('click', () => {
-      menu.classList.toggle('active');
-    });
-  });
+
+    if (toggle && menu) {
+        toggle.addEventListener('click', () => {
+            menu.classList.toggle('active'); 
+        });
+    }
+});
 
 
   
