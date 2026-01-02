8442c93a118b38e6b71fe3db79a2d7cbe2a20280
three-sticks <rhardy2020@gmail.com>
2026-01-01 21:34:36 -0500

    Update scrollReveal.js

--- src/scripts/scrollReveal.js
+++ src/scripts/scrollReveal.js
@@ -1,9 +1,9 @@
 export default function initScrollReveal(targetElements, defaultProps) {
   if (!targetElements.length) return;
 
-  ScrollReveal({ reset: false });
+    const sr = ScrollReveal({ reset: false });
 
   targetElements.forEach(({ element, animation }) => {
-    ScrollReveal().reveal(element, Object.assign({}, defaultProps, animation));
+    sr.reveal(element, Object.assign({}, defaultProps, animation));
   });
 }
